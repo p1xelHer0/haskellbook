9586c23b2e217a1d256ab54fcb19be871d9128e2
Pontus Nagy <pontusnagy@gmail.com>
2018-03-28 07:32:22 +0200

    ch12: wip

--- src/ch12/SmallLibraryForEither.hs
+++ src/ch12/SmallLibraryForEither.hs
@@ -3,9 +3,22 @@ module SmallLibraryForEither where
 lefts' :: [Either a b] -> [a]
 lefts' (Right x:xs) = lefts' xs
 lefts' (Left x:xs) = x : lefts' xs
+lefts' _ = []
+
+rights' :: [Either a b] -> [b]
+rights' (Right x:xs) = x : rights' xs
+rights' (Left x:xs) = rights' xs
+rights' _ = []
 
 -- something something
 lefts'' :: [Either a b] -> [a]
-lefts'' = foldr go [] where
-  go (Right x:xs) = go xs
-  go (Left x:xs) = x : go xs
+lefts'' = undefined
+
+partitionEithers' :: [Either a b] -> ([a], [b])
+partitionEithers' xs = (lefts' xs, rights' xs)
+
+eitherMaybe' :: (b -> c) -> Either a b -> Maybe c
+eitherMaybe' f e = go $ f e where
+  go (Left _) = Nothing
+  go a = Just a
+
